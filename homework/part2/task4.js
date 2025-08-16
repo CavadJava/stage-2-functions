799fe3da4ef1d5888ce1460a0e66692e12e750a6
javad <j.bagirov@expressbank.az>
2025-08-16 16:08:52 +0400

    task4 issue completed ::homework part2

---
+++ homework/part2/task4.js
@@ -0,0 +1,9 @@
+// 4.Find the Index of an Item in an Array
+// a.Find the index of "Pie" in the desserts array.
+//     b.Given: let desserts = ["Ice Cream", "Pie", "Pudding", "Cake"];
+// c.Expected Result: 1
+
+
+let desserts = ["Ice Cream", "Pie", "Pudding", "Cake"];
+
+console.log(desserts.indexOf("Pie"));
